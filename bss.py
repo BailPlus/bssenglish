5672a29351e6c80fee0aab7791550df2369ae65e
Bail <34512765+BailPlus@users.noreply.github.com>
2022-08-17 22:44:02 +0800

    v1.4.1_42
    *修复了linux中缺失的依赖关系(pip)

--- bss.py
+++ bss.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 #coding:utf-8
 #Copyright Bail 2021-2022
-#bssenglish 白杉树背单词训练软件 v1.4_41
-#2021.7.11-2022.8.7
+#bssenglish 白杉树背单词训练软件 v1.4.1_42
+#2021.7.11-2022.8.9
 
 '''
 灵感来源:红杉树智能英语(http://www.hssenglish.com)
