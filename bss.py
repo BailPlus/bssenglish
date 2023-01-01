862098e5dc00c0a3c2e988bfcf7f8498056f63a2
Bail <2915289604@qq.com>
2023-01-01 17:44:18 +0800

    v1.5.8_54 *libclass.py 重命名了文件（原为libwordclass.py） *libfile.py 适配了重命名 *libsc.py 适配了重命名 *libaudio.py 适配了重命名 *libclass.py 将libsc.Sc迁移到此 *libsc.py 迁移Sc类并适配

--- bss.py
+++ bss.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 #coding:utf-8
-#Copyright Bail 2021-2022
-#bssenglish 白杉树背单词训练软件 v1.5.7.1_53
-#2021.7.11-2022.12.8
+#Copyright Bail 2021-2023
+#bssenglish 白杉树背单词训练软件 v1.5.8_54
+#2021.7.11-2023.1.1
 
 '''
 灵感来源:红杉树智能英语(http://www.hssenglish.com)
