7c9f8762b7902490565c1a1993d6474981e3e582
Bail <2915289604@qq.com>
2023-01-01 17:39:18 +0800

    libClass-2 *libclass.py 将libsc.Sc迁移到此 *libsc.py 迁移Sc类并适配

--- libclass.py
+++ libclass.py
@@ -13,4 +13,25 @@ class Word:
             return True
         else:
             return False
-
+class Sc(Word):
+    '''生词类 继承于:单词类'''
+##    learn = wrong = 1	#学习1次，错误1次
+    def __init__(self,word:str,pro:str,trans:str,learn:int,wrong:int,review:int):
+        '''word(str):单词
+pro(str):音标
+trans(str):词义
+review(int※不可为float):复习时间戳'''
+        self.word = word
+        self.pronounce = pro
+        self.trans = trans
+        self.learn = int(learn)
+        self.wrong = int(wrong)
+        self.review = int(review)
+    def strenth(self):
+        '''用于计算记忆强度
+word(Sc):生词对象
+返回值:记忆强度(float:.2f)'''
+        return round((self.learn - self.wrong)/self.learn,2)
+    def items(self):
+        return [self.word,self.pronounce,self.trans,
+                self.learn,self.wrong,self.review]
--- libsc.py
+++ libsc.py
@@ -8,29 +8,6 @@ import time,libclass,libgui,liblist,os,libfile
 ##FN = libfile.getpath('sc')
 remlst = [];lislst = [];wrilst = []
 
-class Sc(libclass.Word):
-    '''生词类 继承于:单词类'''
-##    learn = wrong = 1	#学习1次，错误1次
-    def __init__(self,word:str,pro:str,trans:str,learn:int,wrong:int,review:int):
-        '''word(str):单词
-pro(str):音标
-trans(str):词义
-review(int※不可为float):复习时间戳'''
-        self.word = word
-        self.pronounce = pro
-        self.trans = trans
-        self.learn = int(learn)
-        self.wrong = int(wrong)
-        self.review = int(review)
-    def strenth(self):
-        '''用于计算记忆强度
-word(Sc):生词对象
-返回值:记忆强度(float:.2f)'''
-        return round((self.learn - self.wrong)/self.learn,2)
-    def items(self):
-        return [self.word,self.pronounce,self.trans,
-                self.learn,self.wrong,self.review]
-
 def imp(lst:list):
     '''从外部csv导入生词'''
     newlst = libfile.readfromcsv()
@@ -54,7 +31,7 @@ def readfile():
         lst = eval(f'{i}lst')
         fn = os.path.join(libfile.getpath('data'),f'{i}.csv')
         lst0 = libfile.readfromcsv(fn)
-        lst += [Sc(*i) for i in lst0]
+        lst += [libclass.Sc(*i) for i in lst0]
 def treesort(tree:ttk.Treeview,col:str,reverse:bool):
     print(tree.get_children(''))
     l = [tree.set((k,col),k) for k in tree.get_children('')]
@@ -115,9 +92,9 @@ root(Tk):bss根窗口'''
     writree.heading('复习时间',text='复习时间',command=lambda:treesort(writree,'音标',False))
 
     return (scmain,remtree,listree,writree)
-def reviewtime(obj:Sc):
+def reviewtime(obj:libclass.Sc):
     '''计算到该单词复习时刻的时间
-obj(Sc):生词对象'''
+obj(libclass.Sc):生词对象'''
     if obj.review <= time.time():
         return '0'
     sec = obj.review-time.time()
@@ -188,9 +165,9 @@ word(Sc):生词对象
         return 8*(100*x+100000)
     else:
         raise ValueError('值超出范围')"""
-def deltatime(word:Sc)->int:
+def deltatime(word:libclass.Sc)->int:
     '''计算复习延后秒数
-word(Sc):生词对象
+word(libclass.Sc):生词对象
 返回值:距下次复习秒数(int)'''
     strenth = word.strenth()*100
     x = int(('%.d' % strenth)[-1])
@@ -232,7 +209,7 @@ lst:要存入的列表'''
             i.learn += 1
             i.wrong += 1
             return
-    sc = Sc(word.word,word.pronounce,word.trans,1,1,int(time.time()))
+    sc = libclass.Sc(word.word,word.pronounce,word.trans,1,1,int(time.time()))
     lst.append(sc)
 def get_need_review_list(lst:list):
     '''分出需要复习的词
